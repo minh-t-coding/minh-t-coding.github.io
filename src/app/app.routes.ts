4a4ad713178ea05beded552e9fada6b0d00ae8df
Minh Nguyen <mtnguyen.dev@gmail.com>
2025-11-21 17:25:28 -0700

    added navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/home/home';
+import { Works } from './component/works/works';
+import { Contact } from './component/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Home Page',
+  },
+  {
+    path: 'works',
+    component: Works,
+    title: 'Works',
+  },
+  {
+    path: 'contact',
+    component: Contact,
+    title: 'Contact Me',
+  },
+];
