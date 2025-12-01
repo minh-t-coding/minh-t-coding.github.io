b1314f6065f739f9bc01fd61b1e0fc6d650edcd3
Minh Nguyen <mtnguyen.dev@gmail.com>
2025-11-30 20:53:07 -0700

    added extra projects data

--- src/app/data/projects.data.ts
+++ src/app/data/projects.data.ts
@@ -4,8 +4,26 @@ export const PROJECTS: Project[] = [
     {
         img: 'assets/work/twitter-api.PNG',
         alt: 'Twitter API Screenshot',
+        title: 'Project Management Dashboard',
+        url: 'https://github.com/minh-t-coding/project-management-dashboard',
+        subtitle: 'Full-stack project management dashboard',
+        description: 'Project management dashboard',
+        category: 'web',
+    },
+    {
+        img: 'assets/work/twitter-api.PNG',
+        alt: 'Twitter API Screenshot',
+        title: 'Pokemon Guessing Game',
+        url: 'https://github.com/minh-t-coding/whos-that-pokemon',
+        subtitle: 'Pokemon Guessing Game',
+        description: "Who's that pokemon?",
+        category: 'web',
+    },
+    {
+        img: 'assets/work/twitter-api.PNG',
+        alt: 'Twitter API Screenshot',
         title: 'Twitter Spring API',
-        url: 'https://github.com/fasttrackd-student-work/spring-assessment-social-media-oct-2025-team-3',
+        url: 'https://github.com/minh-t-coding/twitter-spring-api',
         subtitle: 'RESTful Web Service',
         description:
             'Social media backend built with Spring Boot, JPA, and PostgreSQL. Implemented endpoints for users, tweets, hashtags, likes, reposts, replies, and follower relationships. Features include account validation, feed generation, hashtag and mention parsing, and "soft delete" handling to maintain relational integrity.',
@@ -35,7 +53,7 @@ export const PROJECTS: Project[] = [
         img: 'assets/work/beat-drop.png',
         alt: 'BeatDrop Screenshot',
         title: 'BeatDrop',
-        url: 'https://github.com/minh-t-coding/MusicGame',
+        url: 'https://github.com/minh-t-coding/music-game',
         subtitle: 'Physics-based Music Sandbox',
         description:
             'Solo developed Unity physics-based sandbox music game. Players draw notes on a canvas and release a ball to play music based on the surfaces it hits. Maps can be saved, shared, and imported, letting players create and explore intricate musical creations.',
